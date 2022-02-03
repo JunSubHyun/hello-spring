f1754717822f76f2027d39c3b4f4101116a356bf
JunSubHyun <guswnstjq10@gmail.com>
2022-02-03 22:49:16 +0900

    웹 개발 기초  회원 메모리 저장

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -10,10 +10,4 @@ public class HomeController {
     public  String home(){
         return "home";
     }
-
-    @GetMapping("/members/new")
-    public String createForm(){
-        return "members/createMemberForm";
-    }
-
 }
